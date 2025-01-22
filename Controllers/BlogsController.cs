397c95aa1869f5f070c52c390bd68736c398178e
İsmailCan14 <ozturkismailcan11@gmail.com>
2025-01-22 16:31:11 +0300

    Migration ve Details kısmı

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,25 @@
+﻿using blogWeb.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogWeb.Controllers
+{
+
+
+    public class BlogsController : Controller
+    {
+        private readonly BlogDbContext _context;
+        public BlogsController(BlogDbContext context) {  _context = context; }
+        public IActionResult Index()
+        {
+            //var blogs=_context.Blogs.ToList();
+            var blogs =_context.Blogs.Where(x=>x.Status==1).ToList();
+            return View(blogs);
+        }
+
+        public IActionResult Details(int id) 
+        {
+            var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
+            return View(blog);
+        }
+    }
+}
