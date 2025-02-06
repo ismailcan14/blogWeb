ec9f51020059b681d0549bd49c5ce5bb28942880
İsmailCan14 <ozturkismailcan11@gmail.com>
2025-02-06 20:01:54 +0300

    Blog Kaydet, Sil, Geri Dön Aç/Kapa Tuşlarının Atamaları

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using blogWeb.Context;
+using blogWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blogWeb.Controllers
@@ -26,5 +27,42 @@ namespace blogWeb.Controllers
             var blog = _context.Blogs.Where(x=>x.Id==id).FirstOrDefault();  
             return View(blog);
         }
+        public IActionResult DeleteBlog(int id)
+        {
+            var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
+            _context.Blogs.Remove(blog);
+            _context.SaveChanges();
+            return RedirectToAction("Blogs");
+        }
+
+        [HttpPost]
+        public IActionResult EditBlog(Blog model)
+        {
+            var blog = _context.Blogs.Where(x => x.Id == model.Id).FirstOrDefault();
+            blog.Name=model.Name;
+            blog.Description=model.Description;
+            blog.ImageUrl=model.ImageUrl;
+            blog.Tags=model.Tags;
+            blog.LikeCount=model.LikeCount;
+            
+            _context.SaveChanges();
+           return  RedirectToAction("Blogs");
+        }
+
+        public IActionResult ToggleStatus(int id)
+        {
+            var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
+            if(blog.Status==1)
+            {
+                blog.Status=0;
+            }
+            else
+            {
+                blog.Status = 1;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Blogs");
+        }
+
     }
 }
